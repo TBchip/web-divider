3f2c39367bd0500e056b60aa9047097c7eee9702
TBchip <bischoffthijs@gmail.com>
2020-11-26 14:10:06 +0100

    test

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const spdy = require("spdy");
 const httpProxy = require("http-proxy");
 
 const port = 50783;
@@ -8,9 +9,27 @@ const options = {
     "62.131.213.61": "http://192.168.2.1:34699"
 }
 
+
 let proxy = httpProxy.createProxy();
 
 http.createServer(function(req, res) {
     console.log(req.headers.host);
     proxy.web(req, res, { target: options[req.headers.host] });
-}).listen(port);
+}).listen(80);
+
+
+
+const privateKey = fs.readFileSync("/etc/letsencrypt/live/computerkoninguden.nl/privkey.pem", "utf8");
+const certificate = fs.readFileSync("/etc/letsencrypt/live/computerkoninguden.nl/cert.pem", "utf8");
+const ca = fs.readFileSync("/etc/letsencrypt/live/computerkoninguden.nl/chain.pem", "utf8");
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+const httpsServer = spdy.createServer(credentials, function(req, res) {
+    console.log(req.headers.host);
+    proxy.web(req, res, { target: options[req.headers.host] });
+});
+httpsServer.listen(443);

ce2699895d512f7f05870ba8f58511d26dafe413
TBchip <bischoffthijs@gmail.com>
2020-11-26 12:58:14 +0100

    update

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-var http = require("http");
-var httpProxy = require("http-proxy");
+const http = require("http");
+const httpProxy = require("http-proxy");
 
-var proxy = httpProxy.createProxy();
-var options = {  
+const port = 92783;
+const options = {  
     "computerkoninguden.nl": "http://62.131.213.61:6211",
     "62.131.213.61": "http://62.131.213.61:34699"
 }
+
+let proxy = httpProxy.createProxy();
 
 http.createServer(function(req, res) {
     proxy.web(req, res, {
         target: options[req.headers.host]
     });
-}).listen(80);
+}).listen(port);

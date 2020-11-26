2fd95de76abfe89f523070c7e1bee56cfd2a3aa4
TBchip <bischoffthijs@gmail.com>
2020-11-26 13:58:29 +0100

    test

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const httpProxy = require("http-proxy");
 
 const port = 50783;
 const options = {  
-    "https://computerkoninguden.nl": "http://192.168.2.1:6211",
+    "computerkoninguden.nl": "http://192.168.2.1:6211",
     "62.131.213.61": "http://192.168.2.1:34699"
 }
 

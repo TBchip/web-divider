c4270c3f4500f96eb861018c65406e63bbe80fff
TBchip <bischoffthijs@gmail.com>
2020-11-26 15:38:09 +0100

    now also logging time

--- src/logger.js
+++ src/logger.js
@@ -1,5 +1,12 @@
 function logConnection(req, res){
-    console.log(`[${req.connection.remoteAddress}] connected to ${req.headers.host}`);
+    console.log(`[${getDateTime()}][${req.connection.remoteAddress}] connected to ${req.headers.host}`);
+}
+
+function getDateTime(){
+    let date = new Date();
+    let dateStr = date.getDate() + "-" + date.getMonth() + "-" + date.getFullYear();
+    let timeStr = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    return dateStr + " " + timeStr;
 }
 
 module.exports = logConnection;

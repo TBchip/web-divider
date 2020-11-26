683fa09edee1c9c5186b1162e314beafd0602d82
TBchip <bischoffthijs@gmail.com>
2020-11-26 15:22:30 +0100

    also logging to which website

--- src/logger.js
+++ src/logger.js
@@ -1,5 +1,5 @@
 function logConnection(req, res){
-    console.log(`[${req.connection.remoteAddress}] connected`);
+    console.log(`[${req.connection.remoteAddress}] connected to ${req.headers.host}`);
 }
 
 module.exports = logConnection;

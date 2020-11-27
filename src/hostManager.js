961d6be0aa0dd9fa1000fd0ef7937c86b4e8fcaf
TBchip <bischoffthijs@gmail.com>
2020-11-27 10:03:29 +0100

    added semicolloms for consistency

--- src/hostManager.js
+++ src/hostManager.js
@@ -1,11 +1,11 @@
 function parseHost(host){
     if(typeof(host) === "string"){
         if(host.includes("http"))
-            host = host.split("//")[1]
+            host = host.split("//")[1];
         if(host.includes("www."))
-            host = host.replace("www.", "")
+            host = host.replace("www.", "");
         if(host.includes(":"))
-            host = host.split(":")[0]
+            host = host.split(":")[0];
     }else{
         return "undefined";
     }

cffd3d22b632fe95ff2387685c7535a94817b984
TBchip <bischoffthijs@gmail.com>
2020-11-27 10:01:42 +0100

    cleaned up host parsing algorithm

--- src/hostManager.js
+++ src/hostManager.js
@@ -1,14 +1,11 @@
 function parseHost(host){
     if(typeof(host) === "string"){
-        if(host.includes("http")){
+        if(host.includes("http"))
             host = host.split("//")[1]
-        }
-        if(host.includes("www.")){
+        if(host.includes("www."))
             host = host.replace("www.", "")
-        }
-        if(host.includes(":")){
+        if(host.includes(":"))
             host = host.split(":")[0]
-        }
     }else{
         return "undefined";
     }

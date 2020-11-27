a4cf374d3516d708fcb4d0981af55859afd1a084
TBchip <bischoffthijs@gmail.com>
2020-11-27 10:01:01 +0100

    changed host parsing

--- src/hostManager.js
+++ src/hostManager.js
@@ -1,7 +1,14 @@
 function parseHost(host){
     if(typeof(host) === "string"){
-        host = host.replace("www.", "");
-        return host;
+        if(host.includes("http")){
+            host = host.split("//")[1]
+        }
+        if(host.includes("www.")){
+            host = host.replace("www.", "")
+        }
+        if(host.includes(":")){
+            host = host.split(":")[0]
+        }
     }else{
         return "undefined";
     }
